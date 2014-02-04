5b69259fd54e775a1a2ed6933732a52275d62d48
komapuro <iruaandroid@gmail.com>
2014-02-05 02:35:35 +0900

    add image

--- vendor/assets/javascripts/table.js
+++ vendor/assets/javascripts/table.js
@@ -1,12 +1,14 @@
 /**
- * 
+ * table setting
  */
-
-
-
 $(document).ready(function(){
 	console.log("読み込み table");
     $("#table_id").dataTable({
-//    	"bSort": false
+		"bPaginate": true,
+		"bLengthChange": false,
+		"bFilter": true,
+		"bSort": true,
+		"bInfo": true,
+		"bAutoWidth": true
     });
 });
